f043e97801eeb49fe587d5f87c946eb97b2dfe2c
Daniel van Flymen <vanflymen@gmail.com>
2018-01-17 01:13:13 -0500

    Force SSL

--- website/settings.py
+++ website/settings.py
@@ -106,3 +106,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Make sure we're locked down
+if DEBUG is False:
+    SESSION_COOKIE_SECURE = CSRF_COOKIE_SECURE = SECURE_SSL_REDIRECT = True
